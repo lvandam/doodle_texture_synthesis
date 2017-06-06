b1a891e7c58b752cbf15ec0f6160a985c9b3cf3e
rwjoverwater <rwjoverwater@gmail.com>
2017-06-06 15:47:16 +0200

    working with multiple textures and masks

--- vgg19.py
+++ vgg19.py
@@ -33,15 +33,11 @@ class Vgg19:
 
         # Convert RGB to BGR
         red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=rgb_scaled)
-        assert red.get_shape().as_list()[1:] == [224, 224, 1]
-        assert green.get_shape().as_list()[1:] == [224, 224, 1]
-        assert blue.get_shape().as_list()[1:] == [224, 224, 1]
         bgr = tf.concat(axis=3, values=[
             blue - VGG_MEAN[0],
             green - VGG_MEAN[1],
             red - VGG_MEAN[2],
         ])
-        assert bgr.get_shape().as_list()[1:] == [224, 224, 3]
 
         self.conv1_1 = self.conv_layer(bgr, "conv1_1")
         self.conv1_2 = self.conv_layer(self.conv1_1, "conv1_2")
@@ -74,16 +70,16 @@ class Vgg19:
         self.pool5 = self.max_pool(self.conv5_4, 'pool5')
         self.pool5_avg = self.avg_pool(self.conv5_4, 'pool5_avg')
 
-        self.fc6 = self.fc_layer(self.pool5, "fc6")
-        assert self.fc6.get_shape().as_list()[1:] == [4096]
-        self.relu6 = tf.nn.relu(self.fc6)
+        #self.fc6 = self.fc_layer(self.pool5, "fc6")
+        #assert self.fc6.get_shape().as_list()[1:] == [4096]
+        #self.relu6 = tf.nn.relu(self.fc6)
 
-        self.fc7 = self.fc_layer(self.relu6, "fc7")
-        self.relu7 = tf.nn.relu(self.fc7)
+        #self.fc7 = self.fc_layer(self.relu6, "fc7")
+        #self.relu7 = tf.nn.relu(self.fc7)
 
-        self.fc8 = self.fc_layer(self.relu7, "fc8")
+        #self.fc8 = self.fc_layer(self.relu7, "fc8")
 
-        self.prob = tf.nn.softmax(self.fc8, name="prob")
+        #self.prob = tf.nn.softmax(self.fc8, name="prob")
 
         self.data_dict = None
         print(("build model finished: %ds" % (time.time() - start_time)))
